f1f78a65301103dd753533029cf4cd37d456e7bf
jamestthompson3 <jamestthompson3@gmail.com>
2018-07-03 00:16:51 +0300

    Adds back examples

---
+++ examples/src/components/charts/StackedBar.js
@@ -0,0 +1,76 @@
+import React, { Fragment, Component } from 'react'
+import styled from 'styled-components'
+
+import stackedData from '../../data/stackedData.json'
+import { GraphContainer, Snippet } from '../styledComponents'
+import { ChartArea, StackedBar } from 'viiksetjs'
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+`
+const isMobile = window.innerWidth <= 500
+
+class StackedBarExample extends Component {
+  state = {
+    orientation: 'ordinal'
+  }
+  handleChange = e =>
+    e.target.checked
+      ? this.setState({ orientation: 'horizontal' })
+      : this.setState({ orientation: 'ordinal' })
+  render() {
+    const { orientation } = this.state
+    return (
+      <Fragment>
+        <InputContainer>
+          <label>change chart orientation</label>
+          <input type="checkbox" onChange={this.handleChange} />
+        </InputContainer>
+        <GraphContainer>
+          <ChartArea
+            data={stackedData.data}
+            type={orientation}
+            numXTicks={isMobile ? 1 : 4}
+            color="grey"
+            xKey="activity"
+            stroke="grey"
+            nogrid
+            yKey={orientation === 'horizontal' && 'activity'}
+            yTickLabelProps={() => ({ dx: '-3rem', fontSize: 10, strokeWidth: '0.5px' })}
+            noYaxis={orientation !== 'horizontal'}
+          >
+            <StackedBar
+              colors={['#51344D', '#6F5060', '#A78682']}
+              keys={['often', 'sometimes', 'never']}
+            />
+          </ChartArea>
+        </GraphContainer>
+        <Snippet>
+          {`
+        <ChartArea
+            data={stackedData.data}
+            type={orientation}
+            color="grey"
+            xKey="activity"
+            stroke="grey"
+            nogrid
+            yKey={orientation === 'horizontal' && 'activity'}
+            yTickLabelProps={() => ({ dx: '-3rem', fontSize: 10, strokeWidth: '0.5px' })}
+            noYaxis={orientation !== 'horizontal'}
+          >
+            <StackedBar
+              colors={['#51344D', '#6F5060', '#A78682']}
+              keys={['often', 'sometimes', 'never']}
+            />
+          </ChartArea>
+        `}
+        </Snippet>
+      </Fragment>
+    )
+  }
+}
+
+export default StackedBarExample
---
+++ examples/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import IndexPage from './components/IndexPage'
+
+render(<IndexPage />, document.getElementById('root'))
